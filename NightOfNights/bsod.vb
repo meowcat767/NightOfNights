497f795623bbc8ca5db39cccdf80421490e38999
Ben <meowcat@meowcat.site>
2026-07-04 22:18:46 +0100

    real bsod 100%

---
+++ NightOfNights/bsod.vb
@@ -0,0 +1,16 @@
+﻿Public Class bsod
+    Private Sub bsod_Load(Sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.FormBorderStyle = FormBorderStyle.None
+        Me.WindowState = FormWindowState.Maximized
+        Me.TopMost = True
+        Me.BackColor = Color.RoyalBlue
+        Me.KeyPreview = True
+    End Sub
+
+    Private Sub bsod_keyDown(Sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Escape Then
+            MessageBox.Show("cop out.")
+            Application.Exit()
+        End If
+    End Sub
+End Class
